1aa32b62df8eb1f3f8225b5d6a5f2336a48bdeee
knfujii <fujiikuninori@facebook.com>
2013-12-06 05:57:17 +0900

    first commit
    $ rails new . -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Calen::Application.config.secret_key_base = 'ff18f019b5cb6e4c09593492dc99e9388764af38446240268071e4553af5abcb53577c3d287953a7b16d69d337bfc6721afee5d1e26879d8b673fba19aa104ba'
